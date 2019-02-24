73a8926d16a166f86237b04fac92891963fce157
Iris Epple <46895227+iridecent@users.noreply.github.com>
2019-02-24 17:49:03 +0100

    Add parser argument to create csv outputf ile

--- gendoc.py
+++ gendoc.py
@@ -4,10 +4,11 @@ import glob
 import argparse
 import numpy as np
 import pandas as pd
-import pprint
+import csv
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfTransformer
 from collections import Counter
+from collections import defaultdict
 
 # gendoc.py -- Don't forget to put a reasonable amount code comments
 # in so that we better understand what you're doing when we grade!
@@ -26,6 +27,8 @@ parser.add_argument("foldername", type=str,
                     help="The base folder name containing the two topic subfolders.")
 parser.add_argument("outputfile", type=str,
                     help="The name of the output file for the matrix data.")
+parser.add_argument ("csvfile", type=str, default=None,
+                     help="The name of the csv file to calculate cosine similarity.")
 
 args = parser.parse_args()
 
@@ -75,25 +78,46 @@ def create_vectors(folder, m=None):
                         del word
                     else:
                         counts[word] += 1                  # vectorspace dict is updated by count of word in that file
-            vectors[topic+" "+file] = counts               # concatenate subfoldername and filename
+            vectors[topic+" "+file] = counts              # concatenate subfoldername and filename
     return vectors
+
 
 
 
 def get_data_for_matrix(vectors):
     """Modify dictionary of vectors to fit pandas DataFrame"""
-    data = vectors
-    columnlabels = []
-    for k, v in vectors[list(vectors.keys())[0]].items():   # get tokennames out of random file - tokens are the same for each file
-        columnlabels.append(k)
-    rowlabels = []
-    for filename in vectors.keys():
-        rowlabels.append(filename)                          # get filenames as rowlabels
+    all_data = vectors
+    all_columnlabels = []
+    for k, v in sorted(vectors[list(vectors.keys())[0]].items()):   # get tokennames out of random file - tokens are the same for each file
+        all_columnlabels.append(k)
+    all_rowlabels = []
+    for filename in sorted(vectors.keys()):
+        all_rowlabels.append(filename)                          # get filenames as rowlabels
         rows = []
-        for word, count in vectors[filename].items():       # remove wordnames and create list only containing counts in the same order as the columnlabels
+        for word, count in sorted(vectors[filename].items()):       # remove wordnames and create list only containing counts in the same order as the columnlabels
             rows.append(count)
-        data[filename] = rows                               # map list of integers (vector) to corresponding file
-    return data, columnlabels, rowlabels                    #todo: remove dublicate articles
+        all_data[filename] = rows                               # map list of integers (vector) to corresponding file
+    return all_data, all_columnlabels, all_rowlabels
+
+
+
+def remove_duplicate_vectors(all_data, all_columnlabels, all_rowlabels):
+    data = {}
+    for k, v in all_data.items():
+        data[k] = tuple(v)
+    upside_down = defaultdict(list)
+    for name, vect in data.items():
+        upside_down[vect].append(name)
+    dropped_articles = []
+    for vec in upside_down.keys():
+        if len(upside_down[vec])>1:
+            for art in upside_down[vec][1:]:
+                del data[art]
+                dropped_articles.append(art)
+                all_rowlabels.remove(art)
+    columnlabels = all_columnlabels
+    rowlabels = sorted(data.keys())
+    return data, columnlabels, rowlabels, dropped_articles
 
 
 def create_term_document_matrix(data, columns):
@@ -120,7 +144,8 @@ def get_raw_numpy_array(matrix):                            # TrucatedSVD takes 
 
 def create_SVD(array, n):
     svd = TruncatedSVD(n_components=n)
-    svdmatrix = svd.fit_transform(array)
+    svdm = svd.fit_transform(array)
+    svdmatrix = pd.DataFrame(svdm)
     return svdmatrix
 
 
@@ -141,37 +166,45 @@ if args.svddims:
 # THERE ARE SOME ERROR CONDITIONS YOU MAY HAVE TO HANDLE WITH CONTRADICTORY
 # PARAMETERS.
 
+
 print("Writing matrix to {}.".format(args.outputfile))
 
 
 vectors = create_vectors(args.foldername, args.basedims)
-data, columnlabels, rowlabels = get_data_for_matrix(vectors)
+all_data, all_columnlabels, all_rowlabels = get_data_for_matrix(vectors)
+data, columnlabels, rowlabels, dropped_articles = remove_duplicate_vectors(all_data, all_columnlabels, all_rowlabels)
 matrix = create_term_document_matrix(data, columnlabels)
 tfidf = apply_tfidf(matrix)
 tfidf_matrix = create_tfidf_matrix(tfidf, columnlabels, rowlabels)
 raw_array = get_raw_numpy_array(matrix)
 
+print("The following articles got dropped: ", dropped_articles)
+
 np.set_printoptions(suppress=True, linewidth=np.nan, threshold=np.nan)
-pd.set_option("display.width", 10**100000)
+pd.set_option("display.width", 10**1000)
 
 
 if not args.tfidf and not args.svddims:
+    matrix.to_csv(args.csvfile, index=True)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         sys.stdout = open(args.outputfile, "w")
         print(matrix)
 
-if args.tfidf and not args.svddims:
+if args.tfidf and not args.svddims and args.csvfile:
+    print("You can't calculate cosine similarity from tfidf values! No csv produced.")
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         sys.stdout = open(args.outputfile, "w")
         print(tfidf_matrix)
 
-if not args.tfidf and args.svddims:
+if not args.tfidf and args.svddims and args.csvfile:
+    print("You can't calculate cosine similarity from svd matrix! No csv produced.")
     svd = create_SVD(raw_array, args.svddims)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         sys.stdout = open(args.outputfile, "w")
         print(svd)
 
 if args.tfidf and args.svddims:
+    print("You need raw counts to calculate cosine similarity! No csv produced.")
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         sys.stdout = open(args.outputfile, "w")
         print(create_SVD(tfidf, args.svddims))
